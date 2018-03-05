a10cd2d0fd7155262a1488ecf7d6ad9f7feba5b6
EGrayTech <EGrayTech@users.noreply.github.com>
2018-03-04 19:22:09 -0500

    Updated the EntriesRepository with a few summary notes.

--- StaffDirectoryProject/Data/EntriesRepository.cs
+++ StaffDirectoryProject/Data/EntriesRepository.cs
@@ -6,7 +6,18 @@ using CorgiDev.StaffDirectoryProject.Models;
 
 namespace CorgiDev.StaffDirectoryProject.Data
 {
+    /// <summary>
+    /// Repository for activities.
+    /// 
+    /// Note: The code in this class is not to be considered a "best practice" 
+    /// example of how to do data persistence, but rather as workaround for
+    /// not having a database to persist data to. I will switch to a database at a later time.
+    /// </summary>
     public class EntriesRepository
     {
+        /// <summary>
+        /// Returns a collection of entries.
+        /// </summary>
+        /// <returns>A list of entries.</returns>
     }
 }
