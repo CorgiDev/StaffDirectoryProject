d4046fb2e115c8ba610f40f7419fed40ebb639df
EGrayTech <EGrayTech@users.noreply.github.com>
2018-03-04 22:27:54 -0500

    Started to customize the content on the About, Contact and Index pages. Also began work on the HomeController.

--- StaffDirectoryProject/Controllers/HomeController.cs
+++ StaffDirectoryProject/Controllers/HomeController.cs
@@ -3,26 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CorgiDev.StaffDirectoryProject.Data;
+using CorgiDev.StaffDirectoryProject.Models;
 
-namespace StaffDirectoryProject.Controllers
+namespace CorgiDev.StaffDirectoryProject.Controllers
 {
     public class HomeController : Controller
     {
+        private EntriesRepository _entriesRepository = null;
+
+        public HomeController()
+        {
+            _entriesRepository = new EntriesRepository();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Entry> entries = _entriesRepository.GetEntries();
+
+            return View(entries);
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About the Accessibility Help Directory";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Only the CorgiDev website below is a legitamate address at this time.";
 
             return View();
         }
